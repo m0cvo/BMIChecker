fd975e4beea29d18d5ee8da00c92b2f77deed0c2
Nigel <m0cvo@yahoo.co.uk>
2022-02-14 11:27:10 +0000

    Add project files.

---
+++ Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BMIChecker
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int heightInCm = Convert.ToInt32(heightBox.Text);
+            float heightInM = heightInCm / 100.0f;
+            float massInKg = float.Parse(massBox.Text);
+            float BMI = (float)(massInKg / Math.Pow(heightInM, 2));
+            bmiBox.Text = BMI.ToString();
+        }
+    }
+}
